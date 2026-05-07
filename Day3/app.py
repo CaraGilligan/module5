e5e238d8ab23a97e6ccb370f92263e61f95b0cd8
Cara <you@test.com>
2026-05-07 16:22:30 +0100

    day 3 end

---
+++ Day3/app.py
@@ -0,0 +1,21 @@
+class calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+        return self.a + self.b
+    
+    def get_subtract(self):
+        return self.a - self.b
+    
+    def get_multiply(self):
+        return self.a * self.b
+    
+    def get_divide(self):
+        return self.a / self.b
+
+##best practice - put what you're doing in the code under this function 
+if __name__ == "__main__" : 
+    myCalc = calculator(a=2,b=2)
+    print(myCalc.get_sum())
